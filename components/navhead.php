cfad6e4c5ec05fe20c3aa24354758543e67ff723
Jasperr03 <jasperellevera03@gmail.com>
2024-03-07 10:47:02 +0800

    added nav header

---
+++ components/navhead.php
@@ -0,0 +1,6 @@
+<div class="header">
+    <a href="#">
+        <img class="header-logo" src="assets/img/nbsclogo.png" style="height:0.3in;" alt="nbsclogo.png">
+    </a>
+    <p>Northern Bukidnon State College</p>
+</div>
